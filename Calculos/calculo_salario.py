1e57456250e82b4875bcd9411b3ead69f5c7cc1b
Luis Guerra <110359937+sgGuerra@users.noreply.github.com>
2025-02-25 20:27:02 -0500

    Add Calculos, Pruebas
    se agregaron todos los casos de prueba extraordinarios

---
+++ Calculos/calculo_salario.py
@@ -0,0 +1,76 @@
+# Constantes
+SMLV = 1423500  # Salario mínimo legal vigente en 2025
+AUXILIO_TRANSPORTE = 200000  # Auxilio de transporte en 2025
+VALOR_UVT = 49799  # Valor de la Unidad de Valor Tributario en 2025
+PORCENTAJE_SALUD = 0.04  # Porcentaje de descuento de salud
+PORCENTAJE_PENSION = 0.04  # Porcentaje de descuento de pensión
+
+# Bonificaciones por cargo
+BONIFICACIONES_POR_CARGO = {
+    "Empleado nuevo": 50000,
+    "Empleado antiguo": 100000,
+    "Administrador": 150000
+}
+
+# Factores de horas extras
+FACTORES_HORA_EXTRA = {
+    "Diurnas": 1.25,
+    "Nocturnas": 1.75,
+    "Festivas": 2.5
+}
+
+def calcular_bonificacion(cargo):
+    return BONIFICACIONES_POR_CARGO.get(cargo, 0)
+
+def calcular_valor_hora_extra(salario_base, horas_extras, tipo_hora_extra):
+    factor = FACTORES_HORA_EXTRA.get(tipo_hora_extra, 0)
+    return (salario_base / 240) * float(factor) * horas_extras
+
+def calcular_salario_bruto(cargo, salario_base, horas_extras, tipo_hora_extra, horas_extras_adicionales, tipo_hora_extra_adicional):
+    bonificacion = calcular_bonificacion(cargo)
+    valor_hora_extra = calcular_valor_hora_extra(salario_base, horas_extras, tipo_hora_extra)
+
+    if horas_extras_adicionales > 0 and tipo_hora_extra_adicional != "N/A":
+        valor_hora_extra_adiccionales = calcular_valor_hora_extra(salario_base, horas_extras_adicionales, tipo_hora_extra_adicional)
+    else:
+        valor_hora_extra_adiccionales = 0
+    return salario_base + bonificacion + valor_hora_extra + valor_hora_extra_adiccionales
+
+def calcular_deducciones(salario_base, prestamo, cuotas, tasa_interes_anual):
+    salud = salario_base * PORCENTAJE_SALUD
+    pension = salario_base * PORCENTAJE_PENSION
+    if cuotas <= 1:
+        reporte_prestamo = prestamo
+    else:
+        reporte_prestamo = (prestamo * (1 + (tasa_interes_anual / 100))) / cuotas
+
+    return salud + pension + reporte_prestamo
+
+def calcular_impuestos(salario_bruto):
+    limite_inferior = 4 * VALOR_UVT
+    limite_superior = 16 * VALOR_UVT
+
+    if salario_bruto <= limite_inferior:
+        return 0
+    elif salario_bruto <= limite_superior:
+        return (salario_bruto - limite_inferior) * 0.01
+    else:
+        return (limite_superior - limite_inferior) * 0.01 + (salario_bruto - limite_superior) * 0.02
+
+def calcular_nomina(cargo, salario_base, horas_extras=0, tipo_hora_extra="N/A", horas_extras_adicionales=0, tipo_hora_extra_adicional="N/A", prestamo=0, cuotas=0, tasa_interes_anual=6):
+
+    salario_bruto = calcular_salario_bruto(cargo, salario_base, horas_extras, tipo_hora_extra, horas_extras_adicionales, tipo_hora_extra_adicional)
+    deducciones = calcular_deducciones(salario_base, prestamo, cuotas, tasa_interes_anual)
+    impuestos = calcular_impuestos(salario_bruto)
+
+    if horas_extras_adicionales + horas_extras > 30:
+        raise ValueError("La suma de horas extras y adicionales no puede superar 30 horas.")
+
+    if salario_base <= 2 * SMLV:
+        auxilio_transporte = AUXILIO_TRANSPORTE
+    else:
+        auxilio_transporte = 0
+
+    return salario_bruto + auxilio_transporte - deducciones - impuestos
+
+
---
+++ Pruebas/test_salario.py
@@ -0,0 +1,96 @@
+import unittest
+from Calculos.calculo_salario import calcular_nomina
+
+class CalculoSalarioTest(unittest.TestCase):
+    
+    def test_caso_extraordinario_SalarioAlto(self):
+        
+        # Datos entrada
+        cargo = "Administrador"
+        salario_base =  50000000
+        horas_extras = 25
+        tipo_hora_extra = "Festivas"
+        horas_extras_adicionales = 0
+        tipo_hora_extra_adicional = "N/A"
+        prestamo = 20000000
+        cuotas = 40
+        tasa_interes_anual = 6
+        
+        
+        # Datos salida
+        
+        salario_esperado =  57387376.47 
+        
+        resultado = calcular_nomina(cargo, salario_base, horas_extras, tipo_hora_extra, horas_extras_adicionales, tipo_hora_extra_adicional, prestamo, cuotas, tasa_interes_anual)
+        
+        self.assertAlmostEqual(salario_esperado, resultado, 2)
+        
+    def test_caso_extraordinario_MaxHorasExtras(self):
+        
+        # Datos entrada
+        cargo = "Empleado nuevo"
+        salario_base = 1760800
+        horas_extras = 30
+        tipo_hora_extra = "Diurnas"
+        horas_extras_adicionales = 0
+        tipo_hora_extra_adicional = "N/A"
+        prestamo = 0
+        cuotas = 0
+        tasa_interes_anual = 6
+        
+        
+        # Datos salida
+        
+        salario_esperado =  2113302.30 
+
+        
+        resultado = calcular_nomina(cargo, salario_base, horas_extras, tipo_hora_extra, horas_extras_adicionales, tipo_hora_extra_adicional, prestamo, cuotas, tasa_interes_anual)
+        
+        self.assertAlmostEqual(salario_esperado, resultado, 2)
+
+    def test_caso_extraordinario_MaxCuotas(self):
+        # Datos entrada
+        cargo = "Empleado antiguo"
+        salario_base = 2060000
+        horas_extras = 10
+        tipo_hora_extra = "Nocturnas"
+        horas_extras_adicionales = 0
+        tipo_hora_extra_adicional = "N/A"
+        prestamo = 2000000
+        cuotas = 60
+        tasa_interes_anual = 6
+
+        # Datos salida
+
+        salario_esperado =  2273830.63
+
+        resultado = calcular_nomina(cargo, salario_base, horas_extras, tipo_hora_extra, horas_extras_adicionales,
+                                    tipo_hora_extra_adicional, prestamo, cuotas, tasa_interes_anual)
+
+        self.assertAlmostEqual(salario_esperado, resultado, 2)
+
+    def test_caso_extraordinario_HorasExtrasAdiccionales(self):
+
+        # Datos entrada
+        cargo = "Administrador"
+        salario_base = 5670500.00
+        horas_extras = 5
+        tipo_hora_extra = "Diurnas"
+        horas_extras_adicionales = 2
+        tipo_hora_extra_adicional = "Festivas"
+        prestamo = 4000000
+        cuotas = 10
+        tasa_interes_anual = 6
+
+        # Datos salida
+
+        salario_esperado = 5096898.39
+
+        resultado = calcular_nomina(cargo, salario_base, horas_extras, tipo_hora_extra, horas_extras_adicionales,
+                                    tipo_hora_extra_adicional, prestamo, cuotas, tasa_interes_anual)
+
+        self.assertAlmostEqual(salario_esperado, resultado, 2)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
