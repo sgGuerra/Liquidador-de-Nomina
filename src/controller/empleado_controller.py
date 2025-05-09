c53a4251928db54af1a5628ee13200d39a0ddbf6
Luis Guerra <110359937+sgGuerra@users.noreply.github.com>
2025-05-09 14:05:35 +0000

    Se modifico el archivo empleado_controller

--- src/controller/empleado_controller.py
+++ src/controller/empleado_controller.py
@@ -1,28 +1,20 @@
-from model.clase_empleado import Empleado
+import sys
+sys.path.append( "." )
+sys.path.append( "src" )
+
+import psycopg2
+from model.calculo_nomina import Nomina
+import SecretConfig
+
 
 class EmpleadoController:
-    def __init__(self):
-        self.empleados = []  # Lista para almacenar empleados
+    pass
 
-    def agregar_empleado(self, cedula, nombres, apellidos):
-        """
-        Agrega un nuevo empleado a la lista.
-        """
-        empleado = Empleado(cedula, nombres, apellidos)
-        self.empleados.append(empleado)
-        return empleado
 
-    def obtener_empleado_por_cedula(self, cedula):
-        """
-        Busca un empleado por su cédula.
-        """
-        for empleado in self.empleados:
-            if empleado.cedula == cedula:
-                return empleado
-        return None
 
-    def listar_empleados(self):
-        """
-        Devuelve la lista de empleados.
-        """
-        return self.empleados
+
+def Obtener_cursor():
+    connection = psycopg2.connect(database=SecretConfig.PGDATABASE, user=SecretConfig.PGUSER, password=SecretConfig.PGPASSWORD, host=SecretConfig.PGHOST, port=SecretConfig.PGPORT)
+        # Todas las instrucciones se ejecutan a tavés de un cursor
+    cursor = connection.cursor()
+    return cursor
